f90092c0d11c1e955954440fc6f17991e7d8944a
Ektor Makras <ektormakras@gmail.com>
2022-05-04 01:39:10 +0300

    Scroll-Btn Section

---
+++ dist/js/scroll-to-top.js
@@ -0,0 +1,20 @@
+const scroll = document.querySelector('.scroll');
+
+
+window.addEventListener('scroll', () => {
+    scroll.classList.toggle('active', window.scrollY > 200)
+});
+
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
+}
+
+
+scroll.addEventListener('click', () => {
+    scrollToTop();
+});
